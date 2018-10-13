8865b40b55095eb6ba41e515ad865b5b520b10c1
lyxin65 <lyxin65@126.com>
2018-10-13 20:28:53 +0800

    modify steiner tree

--- template/source/miscellany/Steiner-Tree.cpp
+++ template/source/miscellany/Steiner-Tree.cpp
@@ -1,29 +1,47 @@
-priority_queue<pair<int, int> > Q;
-
-// m is key point
-// n is all point
-
-for (int s = 0; s < (1 << m); s++){
-	for (int i = 1; i <= n; i++){
-		for (int s0 = (s&(s-1)); s0 ; s0=(s&(s0-1))){
-				f[s][i] = min(f[s][i], f[s0][i] + f[s - s0][i]);
+void spfa(int state) {
+	while (!q.empty()) {
+		int now = q.front(); q.pop();
+		for (auto v: g[now]) {
+			double len = d[now][v];
+			if (f[v][st[v] | state] > f[now][state] + len) {
+				f[v][st[v] | state] = f[now][state] + len;
+				if ((st[v] | state) != state || vis[v][state])
+					continue;
+				vis[v][state] = true;
+				q.push(v);
 			}
+		}
+		vis[now][state] = false;
 	}
-	for (int i = 1; i <= n; i++) vis[i] = 0;
-    while (!Q.empty()) Q.pop();	
-	for (int i = 1; i <= n; i++){
-		Q.push(mp(-f[s][i], i)); 
-	}       
-	while (!Q.empty()){
-		while (!Q.empty() && Q.top().first != -f[s][Q.top().second]) Q.pop();
-			if (Q.empty()) break;
-			int Cur = Q.top().second; Q.pop();
-			for (int p = g[Cur]; p; p = nxt[p]){
-				int y = adj[p];
-				if ( f[s][y] > f[s][Cur] + 1){
-					f[s][y] = f[s][Cur] + 1;
-					Q.push(mp(-f[s][y], y));
+}
+
+double solve() {
+	int endst = 1 << k;
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 0; j < endst; j++)
+			f[i][j] = 1e18;
+
+	for (int i = 1; i <= n; i++) f[i][st[i]] = 0;
+	for (int j = 1; j < endst; j++) {
+		for (int i = 1; i <= n; i++) {
+			if (!st[i] || (st[i] & j)) {
+				for (int sub = (j-1)&j; sub; sub = (sub-1) & j) {
+					int x = sub | st[i], y = (j - sub) | st[i];
+					update(f[i][j], f[i][x] + f[i][y]);
 				}
 			}
+			if (f[i][j] < 1e18) {
+				q.push(i);
+				vis[i][j] = true;
+			}
+		}
+		spfa(j);
 	}
+
+	double ans = 1e18;
+	for (int i = 1; i <= n; i++) {
+		update(ans, f[i][endst - 1]);
+	}
+	return ans;
 }
